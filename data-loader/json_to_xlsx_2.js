6af44f40ea4692467aa01be117e5a4c2d8b7defd
atageldi194229 <atageldi194229@gmail.com>
2022-11-28 19:22:27 +0500

    asd

---
+++ data-loader/json_to_xlsx_2.js
@@ -0,0 +1,29 @@
+const json2xls = require("json2xls");
+const fs = require("fs");
+const path = require("path");
+
+exports.jsonFilesToOneExcelFile = (json_files, xlsx_file) => {
+  let data = [];
+
+  for (let file of json_files) {
+    data = [...data, ...JSON.parse(fs.readFileSync(file))];
+  }
+
+  data = data.map((e) => ({
+    ...e,
+    entry_time: new Date(e.entry_time),
+    close_time: new Date(e.close_time),
+  }));
+
+  const xls = json2xls(data);
+
+  fs.writeFileSync(xlsx_file, xls, "binary");
+};
+
+let dir = path.join(__dirname, "backtest");
+let files = fs
+  .readdirSync(dir)
+  .filter((e) => e.endsWith(".json"))
+  .map((e) => path.join(dir, e));
+
+exports.jsonFilesToOneExcelFile(files, path.join(dir, "backtest_result.xlsx"));
