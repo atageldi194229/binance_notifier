3dfbae215aca70dbb2f8de85099936ee8eab1364
atageldi194229 <atageldi194229@gmail.com>
2022-11-28 21:48:01 +0500

    asd

--- data-loader/json_to_xlsx_2.js
+++ data-loader/json_to_xlsx_2.js
@@ -5,7 +5,10 @@ const path = require("path");
 exports.jsonFilesToOneExcelFile = (json_files, xlsx_file) => {
   let data = [];
 
+  console.log("started processing");
+
   for (let file of json_files) {
+    console.log(file);
     data = [...data, ...JSON.parse(fs.readFileSync(file))];
   }
 
@@ -22,10 +25,37 @@ exports.jsonFilesToOneExcelFile = (json_files, xlsx_file) => {
   console.log("WORK DONE MY FRIEND");
 };
 
+exports.jsonFilesToExcelFiles = (json_files, dir) => {
+  console.log("started processing");
+
+  for (let file of json_files) {
+    console.log(file);
+    let data = JSON.parse(fs.readFileSync(file));
+
+    data = data.map((e) => ({
+      ...e,
+      entry_time: new Date(e.entry_time),
+      close_time: new Date(e.close_time),
+    }));
+
+    const xls = json2xls(data);
+
+    let xlsx_file =
+      file.split("/").pop().split(".").slice(0, -1).join(".") + ".xlsx";
+    console.log(dir, xlsx_file);
+    fs.writeFileSync(path.join(dir, xlsx_file), xls, "binary");
+
+    console.log("Created: ", xlsx_file);
+  }
+
+  console.log("WORK DONE MY FRIEND");
+};
+
 let dir = path.join(__dirname, "backtest");
 let files = fs
   .readdirSync(dir)
   .filter((e) => e.endsWith(".json"))
   .map((e) => path.join(dir, e));
 
-exports.jsonFilesToOneExcelFile(files, path.join(dir, "backtest_result.xlsx"));
+console.log(process.argv);
+exports.jsonFilesToExcelFiles(files, process.argv[2]);
