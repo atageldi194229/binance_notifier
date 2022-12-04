65794aeed8e9f0bd9013852c828b34d605212a5f
atageldi194229 <atageldi194229@gmail.com>
2022-12-04 20:16:59 +0500

    asfd

--- data-collector/seeders/bot_backtest.js
+++ data-collector/seeders/bot_backtest.js
@@ -89,7 +89,7 @@ class TradeBot {
         {
           //   strategy: "bearish_divergence",
           //   strategy: "bearish_divergence_1-3",
-          //   strategy: "bullish_divergence",
+          strategy: "bullish_divergence",
           //   takeprofit: 2,
           trade_interval: "15m",
           stoploss: {
