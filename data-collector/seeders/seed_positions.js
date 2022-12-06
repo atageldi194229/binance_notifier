327f4d5f74b1f15a06cd22f980cbcf9429655a08
atageldi194229 <atageldi194229@gmail.com>
2022-12-06 21:37:46 +0500

    asd

--- data-collector/seeders/seed_positions.js
+++ data-collector/seeders/seed_positions.js
@@ -7,6 +7,8 @@ let backtest_dir = process.argv[2];
 (async () => {
   let files = fs.readdirSync(backtest_dir).filter((e) => e.endsWith(".json"));
 
+  console.log(files);
+
   for (let i = 0; i < files.length; i++) {
     let file = files[i];
     let data = JSON.parse(fs.readFileSync(path.join(backtest_dir, file)));
