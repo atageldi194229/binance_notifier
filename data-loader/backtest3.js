3f0d64559a1185e76212513e24f7eeb918a85dca
atageldi194229 <atageldi194229@gmail.com>
2022-12-15 22:25:36 +0500

    asd

---
+++ data-loader/backtest3.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { TaskManager } = require("./task_manager");
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+
+const PYTHON_COMMAND = process.env.PYTHON_COMMAND || "python3";
+
+const run_backtest = (file, symbol, interval) => {
+  return new Promise((resolve, reject) => {
+    const pyprog = spawn(PYTHON_COMMAND, [
+      path.join(__dirname, "../tickers_2/backtest2.py"),
+      file,
+      symbol,
+      interval,
+      path.join(__dirname, "backtest"),
+    ]);
+
+    pyprog.stdout.on("data", (data) => console.log(data.toString()));
+    pyprog.stderr.on("data", (data) => console.log(data.toString()));
+    pyprog.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      resolve();
+    });
+  });
+};
+
+const taskManager = new TaskManager(2, () => {
+  console.log("Task manager is empty, all tasks are done");
+  process.exit();
+});
+
+const files = fs
+  .readdirSync(path.join(__dirname, "history"))
+  .filter((e) => e.endsWith(".json"));
+
+for (let file of files) {
+  let [symbol, interval] = file.slice(0, -5).split("_");
+  file = path.join(__dirname, "history", file);
+  console.log(file);
+  taskManager.addTask(() => run_backtest(file, symbol, interval));
+}
---
+++ tickers_2/bactest2.py
@@ -0,0 +1,151 @@
+import sys
+import json
+import numpy as np
+import talib
+import datetime
+
+
+FILE_PATH_5M = sys.argv[1] # 5 minute interval kline's file path
+TRADE_SYMBOL = sys.argv[2]
+TRADE_INTERVAL = sys.argv[3]
+DIR = sys.argv[4]
+
+result = f'{TRADE_SYMBOL}_{TRADE_INTERVAL}\n'
+
+# [[strategy, takeprofit, stoploss, entry_price, entry_time, close_price, close_time, win]]
+
+orders = [];
+closed_orders = [];
+
+
+def create_order(strategy, takeprofit, stoploss, entry_price, entry_time): 
+    orders.append([strategy, takeprofit, stoploss, entry_price, entry_time]);
+
+def close_order(order, close_price, close_time, win, win_percentage):
+    [strategy, takeprofit, stoploss, entry_price, entry_time] = order
+    # closed_orders.append([strategy, takeprofit, stoploss, entry_price, entry_time, close_price, close_time, win, win_percentage])
+    closed_orders.append({
+        "trade_symbol": TRADE_SYMBOL, 
+        "trade_interval": TRADE_INTERVAL,
+        "strategy": strategy, 
+        "takeprofit": takeprofit, 
+        "stoploss": stoploss, 
+        "entry_price": entry_price, 
+        "entry_time": entry_time, 
+        "close_price": close_price, 
+        "close_time": close_time, 
+        "win": win, 
+        "win_percentage": win_percentage
+        })
+
+
+def custom_print(text, end='\n'):
+    global result
+    result = f'{result}{text}{end}'
+    
+
+def print_date_time(ms, end = '\n', print_method = custom_print):
+    s = ms / 1000.0
+    print_method(datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S'), end=end)
+
+# read json file and calculate
+with open(FILE_PATH_5M) as json_file:
+    data_5m = json.load(json_file)
+
+rows = data_5m
+opens = np.array(rows)[:,1]
+opens = list(map(float, opens))
+closes = np.array(rows)[:,4]
+closes = list(map(float, closes))
+highs = np.array(rows)[:,2]
+highs = list(map(float, highs))
+lows = np.array(rows)[:,3]
+lows = list(map(float, lows))
+volumes = np.array(rows)[:,5]
+volumes = list(map(float, volumes))
+
+np_closes = np.array(closes)
+# apo = talib.APO(np_closes, fastperiod=8, slowperiod=21)
+ema21 = talib.EMA(np_closes, timeperiod=21)
+ema8 = talib.EMA(np_closes, timeperiod=8)
+
+# sma volumer
+multiplier1 = 2
+multiplier2 = 4
+
+np_volumes = np.array(volumes)
+v1 = talib.SMA(np_closes, timeperiod=55) 
+v9 = (v1 * multiplier2)
+c = np_volumes > v9
+
+rsi = talib.RSI(np_closes, timeperiod=14)
+
+max_values = []
+min_values = []
+
+rsi_max_values = []
+rsi_min_values = []
+
+last_red_index = 0
+last_green_index = 0
+
+# signal_index = -1
+trend_up_index = -1
+trend_down_index = -1
+bearish_divergence_index = -1
+bullish_divergence_index = -1
+bullish_divergence_ema21_index = -1
+bullish_divergence_then_positive_apo_index = -1
+bearish_divergence_3_index = -1
+bearish_divergence_below_ema21 = -1
+bearish_divergence_below_rsi40 = -1
+macd_price_bullish_index = -1
+ema_4x_above_index = -1
+ema_4x_below_index = -1
+bullish_divergence_canceled_ll_index = -1
+is_last_bullish = False
+last_bullish_for_cancel = 0
+stoploss = 0
+
+is_last_bearish_divergence = 0
+
+green_to_red_indexes = []
+red_to_green_indexes = []
+
+for i in range(1, len(closes)):
+    if c[i] and opens[i] > ema8[i] and closes[i] > ema8[i] and opens[i] < closes[i]:
+        stoploss = round(100 - (( ema21[i] / closes[i] ) * 100), 2)
+        print_date_time(rows[i][0], end=f'   volume_trend_long  {stoploss} \n')
+        create_order("volume_trend_long", 4, stoploss, closes[i], rows[i][6])
+        create_order("volume_trend_long_rsi50_below", 4, stoploss, closes[i], rows[i][6])
+        
+    
+    temp_orders = []
+    for order in orders:
+        [strategy, takeprofit, stoploss, entry_price, entry_time] = order
+        mn = 100 - (lows[i] / entry_price) * 100;
+        mx = 100 - (entry_price / highs[i]) * 100;
+        
+        if strategy == "volume_trend_long":
+            if mn >= stoploss:
+                close_order(order, lows[i], rows[i][6], 0, -stoploss)
+            elif closes[i] < ema21[i]:
+                tp = round(100 - (( closes[i] / order[3] ) * 100), 2)
+                close_order(order, closes[i], rows[i][6], 1, tp)
+            else:
+                temp_orders.append(order)
+                
+        if strategy == "volume_trend_long_rsi50_below":
+            if mn >= stoploss:
+                close_order(order, lows[i], rows[i][6], 0, -stoploss)
+            elif rsi[i] < 50:
+                tp = round(100 - (( closes[i] / order[3] ) * 100), 2)
+                close_order(order, closes[i], rows[i][6], 1, tp)
+            else:
+                temp_orders.append(order)
+    orders = temp_orders
+    
+    
+log_file_name = f"{DIR}/{TRADE_SYMBOL}_{TRADE_INTERVAL}_orders.json"
+with open(log_file_name, "w") as outfile:
+    json.dump(closed_orders, outfile)
