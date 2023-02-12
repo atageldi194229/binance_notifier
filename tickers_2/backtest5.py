20db581eca531b2a934383c7dc8de726f805b830
atageldi194229 <atageldi194229@gmail.com>
2023-02-12 17:15:11 +0300

    upd

--- tickers_2/backtest5.py
+++ tickers_2/backtest5.py
@@ -3,18 +3,18 @@ import json
 import numpy as np
 import talib
 import bot
-import pandas
+import pandas as pd
 import datetime
+
+# # ARGS
+# FILE_PATH = sys.argv[1]
+# OUTPUT_FILE = sys.argv[2]
 
 
 FILE_PATH_5M = sys.argv[1] # 5 minute interval kline's file path
 TRADE_SYMBOL = sys.argv[2]
 TRADE_INTERVAL = sys.argv[3]
 DIR = sys.argv[4]
-
-result = f'{TRADE_SYMBOL}_{TRADE_INTERVAL}\n'
-
-takeprofit = 4
 
 # [[strategy, takeprofit, stoploss, entry_price, entry_time, close_price, close_time, win]]
 orders = [];
@@ -41,59 +41,99 @@ def close_order(order, close_price, close_time, win, win_percentage):
         "win_percentage": win_percentage
         })
 
-def custom_print(text, end='\n'):
-    global result
-    result = f'{result}{text}{end}'
-    
 
-def print_date_time(ms, end = '\n', print_method = custom_print):
-    s = ms / 1000.0
-    print_method(datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S'), end=end)
+
 
 # read json file and data initialization
-with open(FILE_PATH_5M) as json_file:
-    data_5m = json.load(json_file)
+with open(FILE_PATH) as json_file:
+    data = json.load(json_file)
 
-rows = data_5m
-opens = np.array(rows)[:,1]
-opens = list(map(float, opens))
-closes = np.array(rows)[:,4]
-closes = list(map(float, closes))
-highs = np.array(rows)[:,2]
-highs = list(map(float, highs))
-lows = np.array(rows)[:,3]
-lows = list(map(float, lows))
-volumes = np.array(rows)[:,5]
-volumes = list(map(float, volumes))
+df = pd.DataFrame(data, columns=["Open time",
+                                 "Open", 
+                                 "High", 
+                                 "Low", 
+                                 "Close", 
+                                 "Volume", 
+                                 "Close time", 
+                                 "Quote asset volume", 
+                                 "Number of trades", 
+                                 "Taker buy base asset volume", 
+                                 "Taker buy quote asset volume", 
+                                 "Ignore"])
 
-# ema init
-np_closes = np.array(closes)
-ema233 = talib.EMA(np_closes, timeperiod=233)
-ema55 = talib.EMA(np_closes, timeperiod=55)
-ema21 = talib.EMA(np_closes, timeperiod=21)
-ema8 = talib.EMA(np_closes, timeperiod=8)
 
+closes = df["Close"].astype('float64').values
+lows = df["Low"].astype('float64').values
+highs = df["High"].astype('float64').values
+volumes = df["Volume"].astype('float64').values
+
+df["Close"] = closes
+df["Low"] = lows
+df["High"] = highs
+df["Volume"] = volumes
+
+f = lambda ms: datetime.datetime.fromtimestamp(ms / 1000).strftime('%Y-%m-%d %H:%M:%S')
+close_times_string = np.vectorize(f)(df["Close time"].values)
+
+df["Close time text"] = close_times_string
 
 # sma volumer
 multiplier = 3
-np_volumes = np.array(volumes)
-v1 = talib.SMA(np_volumes, timeperiod=20) 
+v1 = talib.SMA(volumes, timeperiod=20) 
 v9 = v1 * multiplier
-c = np_volumes > v9
+df["extremium"] = volumes > v9
+
+# ema init
+ema233 = talib.EMA(closes, timeperiod=233)
+ema55 = talib.EMA(closes, timeperiod=55)
+ema21 = talib.EMA(closes, timeperiod=21)
+ema8 = talib.EMA(closes, timeperiod=8)
+
+# rsi
+rsi = talib.RSI(closes, timeperiod=14)
+df["rsi"] = rsi
+
+df["ema233"] = ema233
+df["ema55"] = ema55
+df["ema21"] = ema21
+df["ema8"] = ema8
 
 
-rsi = talib.RSI(np_closes, timeperiod=14)
-
-max_values = []
-min_values = []
+# ######################
+# rsi max close 70 up
+# ######################
+max_values = [0]
+min_values = [0]
 
 last_red_index = 0
 last_green_index = 0
 
-bearish_divergence_index = -1
 
-for i in range(2, len(closes)):
+df["max values"] = rsi
+for i in range(1, len(df)):
+    df.at[i, 'max values'] = max_values[-1]
 
+    if rsi[i - 1] < 70 and rsi[i] >= 70:
+        last_red_index = i - 1
+        min_value = min(lows[last_green_index: last_red_index + 1])
+        min_values.append(min_value)
+        
+    if rsi[i - 1] > 70 and rsi[i] <= 70:
+        last_green_index = i - 1
+        max_value = max(highs[last_red_index: last_green_index + 1])
+        max_values.append(max_value)
+
+
+
+df["accessible"] = df["max values"] < df["Close"]
+df["accessible"] = df["extremium"] & df["accessible"]
+df["accessible"] = df["accessible"] & df["ema8"] > df["ema21"] 
+df["accessible"] = df["accessible"] & df["ema21"] > df["ema55"] 
+df["accessible"] = df["accessible"] & df["Close"] > df["ema233"]
+
+df = df[df["Close time"] > 1670800799999]
+
+for i in range(1, len(df)):
     temp_orders = []
     for order in orders:
         [strategy, stoploss, ema_type, entry_price, entry_time] = order
@@ -101,26 +141,16 @@ for i in range(2, len(closes)):
         
         if strategy == "extremium_trend":
             if closes[i] < stoploss:
-                close_order(order, lows[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
+                close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl < 0 else 1, pnl)
             elif (ema_type == "ema21" and lows[i] < ema21[i]) or (ema_type == "ema55" and lows[i] < ema55[i]):
-                close_order(order, lows[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
-
-            # if rsi[i] < 45:
-            #     close_order(order, closes[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
+                close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl < 0 else 1, pnl)
 
     orders = temp_orders
 
-
-
-    
-
-    is_seq = ema8[i] > ema21[i] and ema21[i] > ema55[i]
-    if c[i] and closes[i] > ema233[i] and opens[i] < closes[i] and is_seq and len(max_values) >= 1 and max_values[-1] < closes[i] and rsi[i - 1] >= 50 and rsi[i] > 50:
-        rsi50_low_index = i - 1
-        
+    if df["accessible"].iloc[i] and len(orders) == 0:
         is_under_ema21 = False
         is_under_ema55 = False
-        
+
         for j in range(i - 1, 2, -1):
             if rsi[j - 1] < 50:
                 break
@@ -128,24 +158,8 @@ for i in range(2, len(closes)):
                 is_under_ema21 = True
             if lows[j] < ema55[j]:
                 is_under_ema55 = True
-            
-        if not is_under_ema55 and len(orders) == 0:
-            create_order("extremium_trend", ema55[i] if is_under_ema21 else ema21[i], "ema55" if is_under_ema21 else "ema21", closes[i], rows[i][6])
 
-
-    if rsi[i - 1] < 70 and rsi[i] >= 70:
-        last_red_index = i - 1
-
-        min_value = min(lows[last_green_index: last_red_index + 1])
-        min_values.append(min_value)
-        
-        
-    if rsi[i - 1] > 70 and rsi[i] <= 70:
-        last_green_index = i - 1
-
-        max_value = max(highs[last_red_index: last_green_index + 1])
-        max_values.append(max_value)
-
+        create_order("extremium_trend", ema55[i] if is_under_ema21 else ema21[i], "ema55" if is_under_ema21 else "ema21", closes[i], df["Close time text"].iloc[i])
 
 
 log_file_name = f"{DIR}/{TRADE_SYMBOL}_{TRADE_INTERVAL}_orders.json"
---
+++ tickers_2/backtest6.py
@@ -0,0 +1,153 @@
+import sys
+import json
+import numpy as np
+import talib
+import bot
+import pandas
+import datetime
+
+
+FILE_PATH_5M = sys.argv[1] # 5 minute interval kline's file path
+TRADE_SYMBOL = sys.argv[2]
+TRADE_INTERVAL = sys.argv[3]
+DIR = sys.argv[4]
+
+result = f'{TRADE_SYMBOL}_{TRADE_INTERVAL}\n'
+
+takeprofit = 4
+
+# [[strategy, takeprofit, stoploss, entry_price, entry_time, close_price, close_time, win]]
+orders = [];
+closed_orders = [];
+
+
+def create_order(strategy, takeprofit, stoploss, entry_price, entry_time): 
+    orders.append([strategy, takeprofit, stoploss, entry_price, entry_time]);
+
+def close_order(order, close_price, close_time, win, win_percentage):
+    [strategy, takeprofit, stoploss, entry_price, entry_time] = order
+    # closed_orders.append([strategy, takeprofit, stoploss, entry_price, entry_time, close_price, close_time, win, win_percentage])
+    closed_orders.append({
+        "trade_symbol": TRADE_SYMBOL, 
+        "trade_interval": TRADE_INTERVAL,
+        "strategy": strategy, 
+        "takeprofit": takeprofit, 
+        "stoploss": stoploss, 
+        "entry_price": entry_price, 
+        "entry_time": entry_time, 
+        "close_price": close_price, 
+        "close_time": close_time, 
+        "win": win, 
+        "win_percentage": win_percentage
+        })
+
+def custom_print(text, end='\n'):
+    global result
+    result = f'{result}{text}{end}'
+    
+
+def print_date_time(ms, end = '\n', print_method = custom_print):
+    s = ms / 1000.0
+    print_method(datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S'), end=end)
+
+# read json file and data initialization
+with open(FILE_PATH_5M) as json_file:
+    data_5m = json.load(json_file)
+
+rows = data_5m
+opens = np.array(rows)[:,1]
+opens = list(map(float, opens))
+closes = np.array(rows)[:,4]
+closes = list(map(float, closes))
+highs = np.array(rows)[:,2]
+highs = list(map(float, highs))
+lows = np.array(rows)[:,3]
+lows = list(map(float, lows))
+volumes = np.array(rows)[:,5]
+volumes = list(map(float, volumes))
+
+# ema init
+np_closes = np.array(closes)
+ema233 = talib.EMA(np_closes, timeperiod=233)
+ema55 = talib.EMA(np_closes, timeperiod=55)
+ema21 = talib.EMA(np_closes, timeperiod=21)
+ema8 = talib.EMA(np_closes, timeperiod=8)
+
+
+# sma volumer
+multiplier = 3
+np_volumes = np.array(volumes)
+v1 = talib.SMA(np_volumes, timeperiod=20) 
+v9 = v1 * multiplier
+c = np_volumes > v9
+
+
+rsi = talib.RSI(np_closes, timeperiod=14)
+
+max_values = []
+min_values = []
+
+last_red_index = 0
+last_green_index = 0
+
+bearish_divergence_index = -1
+
+for i in range(2, len(closes)):
+
+    temp_orders = []
+    for order in orders:
+        [strategy, stoploss, ema_type, entry_price, entry_time] = order
+        pnl = 100 - (entry_price / closes[i]) * 100
+        
+        if strategy == "extremium_trend":
+            if closes[i] < stoploss:
+                close_order(order, lows[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
+            elif (ema_type == "ema21" and lows[i] < ema21[i]) or (ema_type == "ema55" and lows[i] < ema55[i]):
+                close_order(order, lows[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
+
+            # if rsi[i] < 45:
+            #     close_order(order, closes[i], rows[i][6], 0 if pnl < 0 else 1, pnl)
+
+    orders = temp_orders
+
+
+
+    
+
+    is_seq = ema8[i] > ema21[i] and ema21[i] > ema55[i]
+    if c[i] and closes[i] > ema233[i] and opens[i] < closes[i] and is_seq and len(max_values) >= 1 and max_values[-1] < closes[i] and rsi[i - 1] >= 50 and rsi[i] > 50:
+        rsi50_low_index = i - 1
+        
+        is_under_ema21 = False
+        is_under_ema55 = False
+        
+        for j in range(i - 1, 2, -1):
+            if rsi[j - 1] < 50:
+                break
+            if lows[j] < ema21[j]:
+                is_under_ema21 = True
+            if lows[j] < ema55[j]:
+                is_under_ema55 = True
+            
+        if not is_under_ema55 and len(orders) == 0:
+            create_order("extremium_trend", ema55[i] if is_under_ema21 else ema21[i], "ema55" if is_under_ema21 else "ema21", closes[i], rows[i][6])
+
+
+    if rsi[i - 1] < 70 and rsi[i] >= 70:
+        last_red_index = i - 1
+
+        min_value = min(lows[last_green_index: last_red_index + 1])
+        min_values.append(min_value)
+        
+        
+    if rsi[i - 1] > 70 and rsi[i] <= 70:
+        last_green_index = i - 1
+
+        max_value = max(highs[last_red_index: last_green_index + 1])
+        max_values.append(max_value)
+
+
+
+log_file_name = f"{DIR}/{TRADE_SYMBOL}_{TRADE_INTERVAL}_orders.json"
+with open(log_file_name, "w") as outfile:
+    json.dump(closed_orders, outfile)
