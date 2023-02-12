d440a03c6d14c3ec874527fd44de85fb11837fb3
atageldi194229 <atageldi194229@gmail.com>
2023-02-12 14:55:35 +0300

    upf

--- tickers_2/backtest_extrim.py
+++ tickers_2/backtest_extrim.py
@@ -28,9 +28,6 @@ df = pd.DataFrame(data, columns=["Open time",
                                  "Ignore"])
 
 
-
-
-
 closes = df["Close"].astype('float64').values
 lows = df["Low"].astype('float64').values
 highs = df["High"].astype('float64').values
@@ -47,14 +44,20 @@ v1 = talib.SMA(volumes, timeperiod=20)
 v9 = v1 * multiplier
 c = volumes > v9
 
-df["extrimium"] = c
-
-df.to_csv(OUTPUT_FILE)
-
 # ema init
 ema233 = talib.EMA(closes, timeperiod=233)
 ema55 = talib.EMA(closes, timeperiod=55)
 ema21 = talib.EMA(closes, timeperiod=21)
 ema8 = talib.EMA(closes, timeperiod=8)
 
+df["ema233"] = ema233
+df["ema55"] = ema55
+df["ema21"] = ema21
+df["ema8"] = ema8
+
+df = df[(df["ema8"] > df["ema21"]) & (df["ema21"] > df["ema55"]) & (df["Close"] > df["ema233"])]
+df = df[df["Close time"] > 1670800799999]
+
+# save to csv file
+df.to_csv(OUTPUT_FILE)
 
