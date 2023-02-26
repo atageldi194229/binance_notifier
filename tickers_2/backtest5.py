4c54cecd183a650d6b894437180da53241371f67
atageldi194229 <atageldi194229@gmail.com>
2023-02-26 16:11:52 +0500

    upd

--- tickers_2/backtest5.py
+++ tickers_2/backtest5.py
@@ -187,7 +187,8 @@ for i in range(1, len(df)):
             # if pnl2 > 1:
                 # close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl < 0 else 1, 1)
             if closes[i] < stoploss:
-                close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl <= 0 else 1, 0)
+                pnl2 = 100 - (entry_price / stoploss) * 100
+                close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl2 <= 0 else 1, pnl2)
             elif fastd[i - 1] > 80 and fastd[i] <= 80 and is_bearish_divergence:
                 order[2]="bear_div"
                 close_order(order, lows[i], df["Close time text"].iloc[i], 0 if pnl < 0 else 1, pnl)
@@ -224,7 +225,7 @@ for i in range(1, len(df)):
                 is_under_ema8 = True
 
         # create_order("extremium_trend", ema21[i] if is_under_ema8 else ema8[i], "ema21" if is_under_ema8 else "ema8", closes[i], df["Close time text"].iloc[i])
-        create_order("extremium_trend", closes[i], "ema21" if is_under_ema8 else "ema8", closes[i], df["Close time text"].iloc[i])
+        create_order("extremium_trend", df["max values"].iloc[i], "ema21" if is_under_ema8 else "ema8", closes[i], df["Close time text"].iloc[i])
 
 
 # save result as json file
